7fe931b3a621deb6abc1fafcf617470bf0b62788
Rider Harrison <i0zm6wjx4815r3yb87y6n761152c10@gmail.com>
2019-09-16 10:15:04 -0400

    Initial Commit

---
+++ ScriptingClass/Functions.fs
@@ -0,0 +1,110 @@
+﻿namespace Functions
+
+open System
+open System.Net
+open System.Net.NetworkInformation
+open System.Windows.Forms
+open Microsoft.VisualBasic.Devices
+open System.Management
+open System.Diagnostics
+open System.IO
+open System.Net.NetworkInformation
+open System.Security.AccessControl
+
+
+module PrintStatements = 
+    let PrintHostName () = 
+        let HostName = Environment.MachineName
+        printfn "The system hostname is: %A" HostName
+    
+    let PrintIP() = 
+        let Host = Dns.GetHostEntry(Dns.GetHostName())
+        let IPs = Host.AddressList
+        for i in IPs do
+            if i.AddressFamily = System.Net.Sockets.AddressFamily.InterNetwork then
+                let IPString = i.ToString()
+                printfn "The IP address of the system is %s" IPString      
+
+
+    let PrintDNSServers() = 
+        let DNSServers = NetworkInterface.GetAllNetworkInterfaces()
+
+        for i in DNSServers do
+            let DatBoi = i.GetIPProperties()
+            printfn "%A" DatBoi.DnsAddresses
+
+    let PrintDefaultGateway () = 
+        let adapters  = NetworkInterface.GetAllNetworkInterfaces()
+
+        for adapter in adapters do
+            let adapterProperties = adapter.GetIPProperties()
+            let addresses = adapterProperties.GatewayAddresses
+
+            if addresses.Count > 0 then
+                for address in addresses do
+                    let AddressString = address.Address.ToString()
+                    printfn "%A"  AddressString
+
+    let PrintAvailableMemory() = 
+        let AvailableMemory = int(ComputerInfo().AvailableVirtualMemory)
+        let AvailableInKB = AvailableMemory / 1024
+        let AvailableInMB = AvailableInKB / 1024
+        let AVailableInGB = AvailableInMB / 1024
+        printfn "Free Memory: %AMB" AVailableInGB
+    
+
+    let PrintPath() = 
+        let Path = Environment.CurrentDirectory
+        printfn "Current path: %s" Path
+
+    let  PrintDrivers() = 
+        let WMIObjects = new ManagementObjectSearcher("SELECT * FROM Win32_PnPSignedDriver")
+        for wMIObject in WMIObjects.Get() do
+        
+            try
+                let Device = wMIObject.GetPropertyValue("DeviceName").ToString()
+                printfn "Driver found for: %s" Device
+            with
+            | _ -> printfn ""
+
+    let  PrintProcesses () = 
+        let localAll = Process.GetProcesses()
+        for _process in localAll do
+            printfn "Process: %A    Id: %A" _process.ProcessName _process.Id
+            
+    let KillProcess (x) = 
+        let localAll = Process.GetProcesses()
+        for datboi in localAll do
+            let Id = datboi.Id
+            try 
+                if Id = x then
+                    datboi.Kill()
+                printfn "Process succesfully stopped"
+            with
+            | _ -> printfn "Failed to kill process"
+
+
+    let  PrintActiveConnections () = 
+        let properties = IPGlobalProperties.GetIPGlobalProperties()
+        let connections = properties.GetActiveTcpConnections()
+        for connection in connections do
+            printfn "%s" (connection.LocalEndPoint.ToString() + " <-----> " + connection.RemoteEndPoint.ToString())
+
+module FileInfo = 
+    let ReadTextFile () = 
+        //Read a text file
+        let dialog = new OpenFileDialog()
+        ignore(dialog.ShowDialog())
+        let file = dialog.FileName
+        let filetext =  File.ReadAllText(file)
+        printfn "%s" filetext
+    
+    let  ReadFileSecurity () = 
+        //Read the security of a file
+        let dialog = new OpenFileDialog()
+        ignore(dialog.ShowDialog())
+        let file = dialog.FileName
+        let filesecurity =  File.GetAccessControl(file)
+        printfn "%A" filesecurity
+    
+    
---
+++ ScriptingClass/Program.fs
@@ -0,0 +1,95 @@
+﻿namespace Program
+
+module Main =
+    open System
+    open Functions
+
+    [<EntryPoint>]
+    [<STAThread>]
+    let main argv = 
+    
+
+        while (1 = 1) do 
+            Console.CursorVisible <- false
+            Console.BackgroundColor <- ConsoleColor.Blue
+            Console.ForegroundColor <- ConsoleColor.White
+            Console.Clear()
+
+            let Now = DateTime.Now
+            printfn "__________Programming Project I__________"
+            printfn "__________%A__________" Now
+            printfn "\n"
+
+
+            let Options = [
+                "1: Show System Hostname"
+                "2: Show System IP Address"
+                "3: Show System DNS Servers"
+                "4: Show System Default Gateway"
+                "5: Show Amount of Memory Available to Run Applications"
+                "6: Show PATH enviroment variable"
+                "7: List Drivers Installed On System"
+                "8: List current running tasks"
+                "9: Kill Task"
+                "10: Display a text file on the screen"
+                "11: Get File Security Information"
+                "12: Show All TCP/IP Network Connections and listening Ports"
+                "Press Ctrl + C to exit"
+            ]
+
+
+            for option in Options do
+                printfn "%s" option
+        
+    
+            let mutable SelectedOption = null
+    
+
+            while SelectedOption = null do
+                Console.SetCursorPosition(0,1)
+                let Now = DateTime.Now
+                printfn "__________%A__________" Now
+
+                if Console.KeyAvailable = true then
+                    SelectedOption <- Console.ReadKey().KeyChar.ToString()
+    
+            Console.Clear()
+
+
+            let tryToInt s = 
+                match System.Int32.TryParse s with
+                | true, v ->  v
+                | false, _ -> 0
+
+            if SelectedOption = "1" then
+                PrintStatements.PrintHostName()
+            elif SelectedOption = "2" then 
+                PrintStatements.PrintIP()
+            elif SelectedOption = "3" then
+                PrintStatements.PrintDNSServers()
+            elif SelectedOption = "4" then
+                PrintStatements.PrintDefaultGateway()
+            elif SelectedOption = "5" then
+                PrintStatements.PrintAvailableMemory()
+            elif SelectedOption = "6" then
+                PrintStatements.PrintPath()
+            elif SelectedOption = "7" then
+                PrintStatements.PrintDrivers()
+            elif SelectedOption = "8" then
+                PrintStatements.PrintProcesses()
+            elif SelectedOption = "9" then
+                printfn "*This must be ran as an admin for this function to work*"
+                printfn "Enter a PID: "
+                SelectedOption <- Console.ReadLine().ToString()
+                let PID = tryToInt(SelectedOption)
+                PrintStatements.KillProcess(PID)
+            elif SelectedOption = "10" then
+                FileInfo.ReadTextFile()
+            elif SelectedOption = "11" then
+                FileInfo.ReadFileSecurity()
+            elif SelectedOption = "12" then
+                PrintStatements.PrintActiveConnections()
+
+            ignore(Console.ReadKey())
+        0 // return an integer exit code
+

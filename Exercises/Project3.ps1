d1b765278699e8a4e823b0c24637dd126e659891
Rider Harrison <i0zm6wjx4815r3yb87y6n761152c10@gmail.com>
2019-11-13 10:30:27 -0500

    Finalized version of Project 3 with comments

--- Exercises/Project3.ps1
+++ Exercises/Project3.ps1
@@ -1,10 +1,13 @@
-Clear
-CD "C:\Users\PeeWee2000\Documents\School\ITS 3410 - Scripting For Network Administrators"
-$Data = Get-Content .\STUDENTS.txt
+Clear #Clear the screen
+CD "C:\Users\PeeWee2000\Documents\School\ITS 3410 - Scripting For Network Administrators" #Change directory
+$Data = Get-Content .\STUDENTS.txt #Load data from Students.txt
 
-$Students = $Data -match "\d+-\d"
-$Html = "<table><thead><th></th></thead>"
+$Students = $Data -match "\d+-\d" #Find valid data rows
 
+#Start building a HTML table string by defining the table and header row
+$Html = "<table><thead><tr><th>StudentNumber</th><th>LastName</th><th>FirstName</th><th>DN</th><th>ERN</th><th>HR</th><th>STS</th><th>DEG</th><th>CON</th><th>VER</th><th>GPA</th></tr></thead>"
+
+#Loop through valid data rows and separate data into individual variables via REEgex
 ForEach ($Student in $Students)
 {
     $Student -match "\d+-\d+" | Out-Null
@@ -30,9 +33,8 @@ ForEach ($Student in $Students)
     $Student -match "(?<=\d+-\d+\s\b(\w+\s\w+\.|\w+\s\w+|\w+)\s+(\w+\s\w+\.|\w+\s\w+|\w+)\s+\w\s+\d+\s+\d+\s+\d+\s+\w+\s+\w+\s+\w+\s+)\d\.\d+"| Out-Null
     $GPA = $Matches.0
     $Html = $Html + "<tr>" + "<td>" + $StudentNumber + "</td>" + "<td>" + $LastName + "</td>" + "<td>" + $FirstName + "</td>" + "<td>" + $DN + "</td>" + "<td>" + $ERN + "</td>" + "<td>" + $HR + "</td>" + "<td>" + $STS + "</td>" + "<td>" + $DEG + "</td>" + "<td>" + $CON + "</td>" + "<td>" + $VER + "</td>" + "<td>" + $GPA + "</td>" + "</tr>"
-    Write-Output $GPA
 }
 
-$Html = $Html + "</table>"
-$Html | Out-File -FilePath .\Students.htm
+$Html = $Html + "</table>" #Finish the table
+$Html | Out-File -FilePath .\Students.htm #Pump the HTML string into a text file
 

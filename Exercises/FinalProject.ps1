76dd0528b7ed9de27573d774fe5fa78f1e8284ec
Rider Harrison <i0zm6wjx4815r3yb87y6n761152c10@gmail.com>
2019-11-13 16:14:59 -0500

    Reverted to the non code golf version

--- Exercises/FinalProject.ps1
+++ Exercises/FinalProject.ps1
@@ -1,11 +1,30 @@
-$PrintScriptRunTime =  [system.diagnostics.stopwatch]::StartNew()
-Set-ExecutionPolicy Unrestricted -Force 
-Install-Module ImportExcel -scope CurrentUser 
-foreach ($Row in Import-Excel -Path .\GD2018Price.xlsx -WorkSheetname "CM Pricing" -HeaderName 'Material', 'Description', 'Price') { 
-$CSV1 += $Row.Material + "," + [math]::Round($Row.Price * 0.6,2) + "," + $Row.Price + "`r`n"
-$CSV2 += $Row.Material + "-00," + [math]::Round($Row.Price * 0.6,2) + "," + $Row.Price + "`r`n"
-$CSV3 += $Row.Material + "-01," + [math]::Round($Row.Price * 0.6,2) + "," + $Row.Price + "`r`n" } 
-$CSV1 = "SEP=,`r`nMain Warehouse`r`nMaterial,Cost,Price`r`n" + $CSV1 | Out-File -FilePath .\GDPARTS.CSV
-$CSV2 = "SEP=,`r`nWarehouse-00`r`nMaterial,Cost,Price`r`n" + $CSV2 | Out-File -FilePath .\GDPARTS00.CSV
-$CSV3 = "SEP=,`r`nWarehouse-01`r`nMaterial,Cost,Price`r`n" + $CSV3 | Out-File -FilePath .\GDPARTS01.CSV
+#Rider Harrison ITS 3410 Final Project
+#This script reads an excel workbook and spits out some csv files
+$PrintScriptRunTime =  [system.diagnostics.stopwatch]::StartNew() #Function to time how long the script runs
+Clear #Clear the screen
+CD "C:\Users\PeeWee2000\Documents\School\ITS 3410 - Scripting For Network Administrators"
+Set-ExecutionPolicy Unrestricted -Force #Need admin capabilities
+Install-Module ImportExcel -scope CurrentUser #Install ImportExcel for the current user if it is not already
+
+$WorkSheet = Import-Excel -Path .\GD2018Price.xlsx -WorkSheetname "CM Pricing" -HeaderName 'Material', 'Description', 'Price'
+
+$CSV1 = "SEP=,`r`nMain Warehouse`r`nMaterial,Cost,Price`r`n" #Create a CSV file and set the separator character and header columns
+$CSV2 = "SEP=,`r`nWarehouse-00`r`nMaterial,Cost,Price`r`n" #Create a CSV for Warehouse 00
+$CSV3 = "SEP=,`r`nWarehouse-01`r`nMaterial,Cost,Price`r`n" #Create a CSV for Warehouse 01
+
+foreach ($Row in $WorkSheet) #Loop through all the rows in the worksheet object
+{ 
+    [decimal]$RawCost = $Row.Price * 0.6 #Calculate the cost
+    [decimal]$RoundedCost = [math]::Round($RawCost,2) #Set the Cost to always have 2 decimals
+    
+    #Row.Material is the Material Column for the current Row note that `r`n is recognized a new line
+    $CSV1 += $Row.Material + "," + $RoundedCost + "," + $Row.Price + "`r`n" #Add a data row to CSV1
+    $CSV2 += $Row.Material + "-00," + $RoundedCost + "," + $Row.Price + "`r`n" #Add a data Row to CSV2
+    $CSV3 += $Row.Material + "-01," + $RoundedCost + "," + $Row.Price + "`r`n" #Add a data Row to CSV3
+} 
+
+$CSV1 | Out-File -FilePath .\GDPARTS.CSV #Output CSV1
+$CSV2 | Out-File -FilePath .\GDPARTS00.CSV #Output CSV2
+$CSV3 | Out-File -FilePath .\GDPARTS01.CSV #Output CSV3
+
 $PrintScriptRunTime

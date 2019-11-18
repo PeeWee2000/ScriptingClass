ba867edc701a3a302f8d4895da0976527f0bf7da
Rider Harrison <i0zm6wjx4815r3yb87y6n761152c10@gmail.com>
2019-11-18 12:15:07 -0500

    Fixed exercise 6

--- Exercises/Exercise6-P2.ps1
+++ Exercises/Exercise6-P2.ps1
@@ -1,30 +1,28 @@
-﻿Write-Host "Please don't enter nulls"
-[char]$input
+﻿Clear
+function GetNonNullValue ($TextToWrite) 
+{    
+[string]$Data
+	while (1 -eq 1)
+	{
+		$Data = Read-Host $TextToWrite
+		if (($Data -eq $null) -or ($Data -eq "")) { Write-Host "Null value entered, try again" }
+        else { break }
+	}
+	return $Data
+}
+
 DO 
 {
+    [string]$Date = GetNonNUllValue("Type a date")
+    [string]$Name = GetNonNUllValue("Type a name")
+    [string]$Address = GetNonNUllValue("Type an address")
+    [string]$Phone = GetNonNUllValue("Type a phone number")
+    [string]$City = GetNonNUllValue("Type a City")
+    [string]$State = GetNonNUllValue("Type a state")
+    [string]$Zip = GetNonNUllValue("Type a zip")
 
-Clear
-[string]$Date = Read-Host "Type a date"
-[string]$Name = Read-Host "Type a name"
-[string]$Address = Read-Host "Type an address"
-[string]$Phone = Read-Host "Type a phone number"
-[string]$City = Read-Host "Type a City"
-[string]$State = Read-Host "Type a state"
-[string]$Zip = Read-Host "Type a zip"
+	($Date + "  " + $Name + "  " + $Address + "  " + $Phone + "  " + $City + "  " + $State + "  " + $Zip + "`r`n") | Out-File -Append -FilePath .\DatBoi.txt 
 
+	$input = Read-Host "Type Q and press enter to Quit or press the any other key to continue"		
 
-if ([string]::IsNullOrEmpty($Date) -or [string]::IsNullOrEmpty($Name) -or [string]::IsNullOrEmpty($Address) -or [string]::IsNullOrEmpty($Phone) -or [string]::IsNullOrEmpty($City) -or [string]::IsNullOrEmpty($State) -or [string]::IsNullOrEmpty($Zip))
-	{
-		Write-Host "Null values were entered record not written"
-		$input = Read-Host "Type Q and press enter to Quit or press the any other key to continue"
-	}
-else
-	{ 
-		($Date + $Name + $Address + $Phone + $City + $State + $Zip) | Out-File -Append -FilePath .\DatBoi.txt 
-		$input = Read-Host "Type Q and press enter to Quit or press the any other key to continue"		
-	}
 } While ($input -notlike "Q") 
-
-
-
-
